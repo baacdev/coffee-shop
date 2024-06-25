36dca50d30b99a68f7a42bda293af7dd08e20f3f
baac <brandonacosta600@gmail.com>
2024-06-25 17:44:08 -0500

    Now the images will be in the public/assets/img folder and the logos in public/assets/logos. Language change from spanish to english in HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const promociones = [
+const promotions = [
   {
     id: 1,
-    name: 'Vuélvete experto en el café desde casa',
-    description: 'Conoce los métodos de café y lleva a casa tu favorito.',
+    name: 'Become a coffee expert from home',
+    description: 'Get to know the coffee methods and take home your favorite.',
     promotionStartDate: '',
     promotionEndDate: '',
     href:'/ecommerce/section/menu/merch-y-cafe-en-grano/merch',
-    buttonName: 'Comprar ahora',
-    imageSrc: 'https://prodappconfiguration.blob.core.windows.net/bannersimages/PRE3.png'
+    buttonName: 'Buy now',
+    imageAlt: 'A coffee maker, tea maker, and glass pitcher arranged on a countertop.',
+    imageSrc: '/assets/img/electrodomesticos.jpeg'
   },
   {
     id: 2,
-    name: '¿Aún no eres miembro Rewards y deseas el nuevo Stanley?',
-    description: 'Regístrate y disfruta de nuestra preventa exclusiva solo hoy y mañana.',
+    name: 'Not yet a Rewards member and want the new tanley stainless steel water bottle?',
+    description: 'Register and enjoy our exclusive pre-sale.',
     promotionStartDate: '',
     promotionEndDate: '',
     href:'/account/register',
-    buttonName: 'Regístrarme',
-    imageSrc: 'https://prodappconfiguration.blob.core.windows.net/bannersimages/ROSA3.png'
+    buttonName: 'Sign up',
+    imageAlt: 'Stanley stainless steel water bottle: a durable and sleek hydration solution for on-the-go adventures.',
+    imageSrc: '/assets/img/stanley-termo.png'
   },
   {
     id: 3,
-    name: 'Despierta el día con energía y sabor',
-    description: 'Pide en tienda tu combinación perfecta para iniciar el día con alegría y buen gusto.',
+    name: 'Wake up the day with energy and flavor',
+    description: 'Order in store your perfect combination to start the day with joy and good taste.',
     promotionStartDate: '01-04-2024',
     promotionEndDate: '16-09-2024',
     href:'/ecommerce/section/menu',
-    buttonName: 'Conoce más aquí',
-    imageSrc: 'https://prodappconfiguration.blob.core.windows.net/bannersimages/BA1.png'
+    buttonName: 'Learn more',
+    imageAlt: 'Caramel apple latte in a Starbucks cup, topped with whipped cream and caramel drizzle.',
+    imageSrc: '/assets/img/caremel_macchiato_bananaa_bread.jpg'
   },
   {
     id: 4,
-    name: 'Llegó la temporada Retro',
-    description: 'Disfruta nuestro delicioso Frappuccino sin café de vainilla con mango dragon fruit.',
+    name: 'Retro season is here',
+    description: 'Enjoy our delicious coffee-free vanilla Frappuccino with mango dragon fruit.',
     promotionStartDate: '09-02-2024',
     promotionEndDate: '21-07-2024',
     href:'/ecommerce/section/menu/bebidas/frappuccinos',
-    buttonName: 'Conoce más aquí',
-    imageSrc: 'https://prodappconfiguration.blob.core.windows.net/bannersimages/RETRO1.png'
+    buttonName: 'Learn more',
+    imageAlt: 'Two glasses of pink milkshake topped with whipped cream, served in clear glassware.',
+    imageSrc: '/assets/img/frappuccino-rosa.jpg'
   },
 ]
 
@@ -49,7 +53,7 @@ function HomePage() {
     <>
       <div className='bg-white flex min-h-[100vh]'>
         <div className='bg-white mt-10 mb-10 mx-auto max-w-2xl flex flex-col items-center gap-y-16 lg:max-w-7xl'>
-          {promociones.map((promocion) => (
+          {promotions.map((promocion) => (
             <div key={promocion.id} className='bg-stone-100 flex flex-col lg:flex-row items-center lg:pb-0'>
               <div className='text-center m-1 lg:m-0 lg:pl-10 lg:text-left lg:w-1/2'>
                 <h2 className='mt-3 text-2xl font-bold font-opensans tracking-tight text-orange-900 sm:text-5xl sm:mt-0'>
@@ -71,7 +75,7 @@ function HomePage() {
               <div className='w-full h-full mt-5 lg:mt-0 lg:ml-10 lg:w-1/2 flex justify-center'>
                 <img
                   src={promocion.imageSrc}
-                  alt='Imagen de promoción'
+                  alt={promocion.imageAlt}
                   className='rounded-none bg-gray-100 w-full h-full'
                 />
               </div>
